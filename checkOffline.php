b222693cfc72bcf541881317030268b1b8bdc6c6
Craig Ballingall <cdwb1@st-andrews.ac.uk>
2024-04-16 21:46:05 +0100

    Add cors headers

--- checkOffline.php
+++ checkOffline.php
@@ -1,6 +1,8 @@
 <?php
 // A request to this page will return 1 if the system is offline.
 // Otherwise, it will return 0.
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET');
 
 $config = require 'config.php';
 
